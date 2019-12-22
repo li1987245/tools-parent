175082d2f6e6ec7a55e14a6739ffde0458fbc1c0
li_1987245 <li_1987245@163.com>
2019-12-22 20:39:15 +0800

    init

---
+++ tools-dataframe/readme.md
@@ -0,0 +1,4 @@
+- scala 打包
+```
+mvn clean scala:compile  compile
+```
---
+++ tools-dataframe/src/test/scala/io/github/tools/dataframe/CollectionTest.scala
@@ -0,0 +1,10 @@
+package io.github.tools.dataframe
+
+import org.junit.Test
+
+class CollectionTest {
+  @Test
+  def testBitMap(): Unit = {
+    println("bit map")
+  }
+}
